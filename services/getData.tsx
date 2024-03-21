927c6e3c3db36db0c057581805276418ed08deca
Devang1224 <97470914+Devang1224@users.noreply.github.com>
2024-03-21 18:14:12 +0530

    initial commit

---
+++ services/getData.tsx
@@ -0,0 +1,40 @@
+import avatar from "../public/assets/avatar-1-msCO90xk.ico"
+
+const getData = [
+    {
+        image:avatar,
+        name:'Lorem Ipsum',
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, atque corrupti recusandae ab beatae cupiditate rerum incidunt ea quis laborum!",
+        likes:'2k',
+        views:'2k',
+        comments:'3k',
+        lastVisited:'2 min ago'
+    },
+    {
+        image:avatar,
+        name:'Lorem Ipsum',
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, atque corrupti recusandae ab beatae cupiditate rerum incidunt ea quis laborum!",
+        likes:'2k',
+        views:'2k',
+        comments:'3k',
+        lastVisited:'2 min ago'
+    }, {
+        image:avatar,
+        name:'Lorem Ipsum',
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, atque corrupti recusandae ab beatae cupiditate rerum incidunt ea quis laborum!",
+        likes:'2k',
+        views:'2k',
+        comments:'3k',
+        lastVisited:'2 min ago'
+    }, {
+        image:avatar,
+        name:'Lorem Ipsum',
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, atque corrupti recusandae ab beatae cupiditate rerum incidunt ea quis laborum!",
+        likes:'2k',
+        views:'2k',
+        comments:'3k',
+        lastVisited:'2 min ago'
+    },
+]
+
+
